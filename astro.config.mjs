86f9177ddd69b3558ab68034d7e40e37b9f3c06a
miksin <contact@miksin.art>
2025-01-12 04:34:02 +0000

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import vue from '@astrojs/vue';
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  integrations: [vue()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    }
+  })
+});
