a8756742ad7a10f1f445dd8a8dc3f1b3373d7ff3
miksin <contact@miksin.art>
2025-01-21 22:24:44 +0900

    optimize imports

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,10 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  plugins: [
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-organize-imports",
+  ],
   overrides: [
     {
       files: "*.astro",
