c9013a30cde9f53f424effad89f2e77bcbd423e7
limin <contact@miksin.art>
2025-01-22 23:25:47 +0900

    book creation (#13)

--- src/components/CreateBookDialog.vue
+++ src/components/CreateBookDialog.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import SVGIcon from "@components/SVGIcon.vue";
+import { currencyList, thumbnailList } from "@lib/constants";
+import { BookStatus, type Book } from "@lib/models";
+import { ulid } from "ulid";
+import { useMutation } from "@lib/store";
 
 const dialogOpen = ref(false);
 function openDialog() {
@@ -8,6 +12,42 @@ function openDialog() {
 }
 function closeDialog() {
   dialogOpen.value = false;
+}
+
+const nameValue = ref("");
+const currencyValue = ref(currencyList[0]);
+const thumbnailValue = ref(thumbnailList[0]);
+
+const clearForm = () => {
+  nameValue.value = "";
+  currencyValue.value = currencyList[0];
+  thumbnailValue.value = thumbnailList[0];
+};
+
+const { trigger, isLoading } = useMutation<void>();
+
+const canSubmit = computed(() => {
+  if (nameValue.value.length < 1) return false;
+  if (nameValue.value.length > 32) return false;
+  if (isLoading.value) return false;
+  return true;
+});
+
+async function handleSubmit() {
+  const formData: Book = {
+    ulid: ulid(),
+    name: nameValue.value,
+    currency: currencyValue.value,
+    thumbnail: thumbnailValue.value,
+    status: BookStatus.Active,
+  };
+  await trigger("/api/books", {
+    method: "POST",
+    body: JSON.stringify(formData),
+  });
+  clearForm();
+  // TODO: update cache
+  closeDialog();
 }
 </script>
 
@@ -29,19 +69,50 @@ function closeDialog() {
       <form class="flex flex-col gap-4">
         <label class="form-control">
           <div class="label">
-            <span class="label-text">Title</span>
+            <span class="label-text">Currency</span>
           </div>
-          <input type="text" class="input input-secondary" />
+          <select class="select select-secondary" v-model="currencyValue">
+            <option v-for="currency in currencyList" :value="currency">
+              {{ currency }}
+            </option>
+          </select>
+        </label>
+        <label class="form-control">
+          <div class="label">
+            <span class="label-text">Name</span>
+            <span class="label-text-alt">{{ nameValue.length }}/32</span>
+          </div>
+          <input
+            type="text"
+            v-model="nameValue"
+            class="input input-secondary"
+            maxlength="32"
+          />
         </label>
         <label class="form-control">
           <div class="label">
             <span class="label-text">Thumbnail</span>
           </div>
-          <select class="select select-secondary">
-            <option value="home">Home</option>
-          </select>
+          <div class="grid autofill-grid gap-2">
+            <div
+              v-for="iconName in thumbnailList"
+              class="rounded-lg cursor-pointer aspect-square bg-secondary flex items-center justify-center"
+              :class="{ 'ring-4 ring-primary': iconName === thumbnailValue }"
+              @click="thumbnailValue = iconName"
+            >
+              <SVGIcon :name="iconName" class="w-3/4 h-3/4 fill-base-100" />
+            </div>
+          </div>
         </label>
-        <button type="button" class="btn btn-primary">Create</button>
+        <button
+          type="button"
+          class="btn btn-primary"
+          :disabled="!canSubmit"
+          @click="handleSubmit"
+        >
+          <span v-if="isLoading" class="loading loading-spinner"></span>
+          <template v-else> Create </template>
+        </button>
       </form>
     </div>
     <form method="dialog" class="modal-backdrop">
@@ -49,3 +120,9 @@ function closeDialog() {
     </form>
   </dialog>
 </template>
+
+<style>
+.autofill-grid {
+  grid-template-columns: repeat(auto-fill, minmax(3rem, 1fr));
+}
+</style>
--- src/lib/models.ts
+++ src/lib/models.ts
@@ -1,8 +1,9 @@
 export type Book = {
   ulid: string;
   name: string;
-  currency: string;
-  status: number;
+  currency: Currency;
+  thumbnail: string; // TODO: add to schema
+  status: BookStatus;
 };
 
 export type UserBookRel = {
@@ -10,3 +11,16 @@ export type UserBookRel = {
   bookUlid: string;
   sum: number;
 };
+
+export const BookStatus = {
+  Archived: 0,
+  Active: 1,
+} as const;
+export type BookStatus = (typeof BookStatus)[keyof typeof BookStatus];
+
+export const Currency = {
+  JPY: "JPY",
+  TWD: "TWD",
+  USD: "USD",
+} as const;
+export type Currency = (typeof Currency)[keyof typeof Currency];
